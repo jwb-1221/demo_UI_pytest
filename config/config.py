061b3d758646a479040ddfaad43cd50cb4845586
JWB <779510476@qq.com>
2021-04-28 18:29:25 +0800

    优化

---
+++ config/config.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+from PIL import ImageGrab
+import time
+import sys
+import lib
+class config(object):
+    "配置信息"
+    def config(self):
+        "配置浏览器信息"
+        self.dr = webdriver.Chrome()
+        self.dr.implicitly_wait(10)
+        self.dr.maximize_window()
+    def config_Chrome(self):
+        pass
+    def config_Firefox(self):
+        self.dr = webdriver.Firefox(executable_path= )
+
+
+
+
+
---
+++ run.py
@@ -0,0 +1,3 @@
+import time
+current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+print("开始测试时间:",current_time)
