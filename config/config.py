6670f6facb36a3a7f960f0ba8ef930cf96041c59
JWB <779510476@qq.com>
2021-05-08 16:17:54 +0800

    优化

--- config/config.py
+++ config/config.py
@@ -1,48 +1,11 @@
-from selenium import webdriver
-from config import URL
-import time
 import sys,os
 config = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(config)
+#配置
+CONFIG = os.path.join(config,"config","config.ini")
+#HTML报告路径
+report = os.path.join(config,"report")
 #截图路径
-scrrrnshot_report = os.path.join(config,"report"+"/")
-
-class config(object):
-    "浏览器配置信息"
-    def config(self):
-        "配置浏览器信息"
-        self.dr.implicitly_wait(10)
-        self.dr.maximize_window()
-
-    def config_Chrome_admin(self):
-        """打开谷歌浏览器
-        进入运营端
-        隐式等待为10秒"""
-        self.dr = webdriver.Chrome()
-        config.config(self)
-        self.dr.get(URL.URL.admin_url(self))
-
-    def config_Chrome_merchant(self):
-        """打开谷歌浏览器
-        进入商户端
-        隐式等待为10秒"""
-        self.dr = webdriver.Chrome()
-        config.config(self)
-        self.dr.get(URL.URL.merchant_url(self))
-
-    def config_Firefox_admin(self):
-        """打开火狐浏览器
-        进入运营端
-        隐式等待为10秒"""
-        self.dr = webdriver.Firefox()
-        config.config(self)
-        self.dr.get(URL.URL.admin_url(self))
-
-    def config_Firefox_merchant(self):
-        """打开火狐浏览器
-        进入运营端
-        隐式等待为10秒"""
-        self.dr = webdriver.Firefox()
-        config.config(self)
-        self.dr.get(URL.URL.merchant_url(self))
-
+TEST_REPORT = os.path.join(config,"screenshot")
+#测试文件
+TEST_CASE = os.path.join(config,"testCase")
---
+++ testCase/test.py
@@ -0,0 +1,13 @@
+import time
+from config import config
+from lib import screenshot
+from common import browser
+import unittest
+class TEST_1(unittest.TestCase):
+    def test_1(self):
+        browser.config.config_Chrome_admin(self)
+        time.sleep(2)
+        screenshot.scrrrnshot("登录").ImageGrab()
+    def test_2(self):
+        config.config.config_Firefox_admin(self)
+        time.sleep(10)
