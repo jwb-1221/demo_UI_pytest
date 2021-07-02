87822a5ee0208c61726b274ec9cb55be6e9f7661
JWB <779510476@qq.com>
2021-07-02 09:50:13 +0800

    pytest

--- testCase/test.py
+++ testCase/test.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 class Testcase(object):
     def setup_class(self):
         print("类级别的 setup")
@@ -13,13 +14,17 @@ class Testcase(object):
         print('类方法级别的 setup.....')
 
     # @pytest.mark.skip(condition='我就是要跳过这个用例啦')
+    @allure.title('测试用例标题1')
+    @allure.description('这是测试用例用例1的描述信息')
     def test_case01(self):
 
         print('执行用例01.......')
 
         assert 0  # 断言失败
 
-    @pytest.mark.skipif(condition=1<2, reason='如果条件为true就跳过用例')
+    # @pytest.mark.skipif(condition=1<2, reason='如果条件为true就跳过用例')
+    @allure.title('测试用例标题2')
+    @allure.description('这是测试用例用例2的描述信息')
     def test_case02(self):
         print('执行用例02.......')
 
