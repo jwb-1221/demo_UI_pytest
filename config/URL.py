762a7e4e0c40ecabfc6b9763a19c490368df7477
JWB <779510476@qq.com>
2021-07-02 18:02:03 +0800

    pytest

--- config/URL.py
+++ config/URL.py
@@ -1,7 +1,7 @@
 class URL():
     def merchant_url(self):
         """商户端地址"""
-        return "http://account-merchant-webos-test.lastmiles.cn/#/"
+        return "*************************"
     def admin_url(self):
         """运营端地址"""
-        return "http://account-admin-webos-test.lastmiles.cn/#/"
+        return "*************************"
